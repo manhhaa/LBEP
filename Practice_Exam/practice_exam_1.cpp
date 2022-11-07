d0d83f691e26be436d8e87a5a3578886a4a53303
manhhaa <hanguyenns793@gmail.com>
2022-11-07 10:06:59 +0700

    change

--- Practice_Exam/practice_exam_1.cpp
+++ Practice_Exam/practice_exam_1.cpp
@@ -7,17 +7,8 @@ void sortNum() {
 		printf("a[%d]: ", i+1);
 		scanf("%d",&a[i]);
 	}
-	for(int i=0; i<10; i++) {
-		for(int j=i+1; j<10; j++) {
-			if(a[i]>a[j]) {
-				int temp = a[i];
-				a[i] = a[j];
-				a[j]=temp;
-			}
-		}
-	}
 	printf("Display in the reversed order\n");
-	for(int i = 0; i<10; i++) {
+	for(int i = 9; i>=0; i--) {
 		printf("%d\n",a[i]);
 	}
 }
